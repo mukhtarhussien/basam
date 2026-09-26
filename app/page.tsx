0deb2b7356424db8cbcdbd73472386fe9655807b
Mukhtar hussien- مختار حسين <za.smoke55@gmail.com>
2026-09-26 23:18:54 +0300

    Remove hidden verification challenge from page
    Removed hidden verification challenge string from the page.

--- app/page.tsx
+++ app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
     <main className="min-h-screen bg-background text-foreground p-4 flex flex-col gap-5">
       {/* KillMyWeb verification challenge */}
       <span hidden>
-        gOMwdPslug8PowKZ9iMFKmLKSj4ZOGQL1RFO2VfsgAFs7mwX
+        7OyhQSTMAqQsUIQaRnvgnNBbE8zC1e4xK1AzmCljciOi7O3r
       </span>
 
       <div className="flex items-center gap-3">
